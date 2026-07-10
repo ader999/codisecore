51cf50c0275f02d73e1445cceb757107f5f383cb
ader999 <ader999@example.com>
2026-07-09 22:00:06 -0600

    conectando base de datos postgresql

--- codiselu/settings.py
+++ codiselu/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'codiselu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'zl39puxfl9fiqb9v',
+        'HOST': 'cdice-codiseludb-wedvsj-270e2e-178-104-105-241.sslip.io',
+        'PORT': '5432',
     }
 }
 
